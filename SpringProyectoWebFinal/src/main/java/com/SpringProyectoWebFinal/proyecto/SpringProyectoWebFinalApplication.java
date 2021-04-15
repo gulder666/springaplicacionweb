4c2e2abab30a8cff1631178b29753e915f1427d8
DONAL <DONAL@DESKTOP-7EB9P4J>
2021-04-15 02:01:39 -0500

    LOGULA COMMITS

---
+++ SpringProyectoWebFinal/src/main/java/com/SpringProyectoWebFinal/proyecto/SpringProyectoWebFinalApplication.java
@@ -0,0 +1,13 @@
+package com.SpringProyectoWebFinal.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectoWebFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyectoWebFinalApplication.class, args);
+	}
+
+}
---
+++ SpringProyectoWebFinal/src/test/java/com/SpringProyectoWebFinal/proyecto/SpringProyectoWebFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringProyectoWebFinal.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProyectoWebFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
